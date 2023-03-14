8b02c83d5f6983b6185d07be28da10e7e4e14798
Eduarda Garangau <eduardagarangau98@gmail.com>
2023-03-14 13:45:43 -0300

    create positioned message bubble

--- design_system/lib/widgets/message_bubble_widget.dart
+++ design_system/lib/widgets/message_bubble_widget.dart
@@ -18,39 +18,45 @@ class MessageBubbleWidget extends StatelessWidget {
     final themeColor = Theme.of(context).extension<ThemeColorsExtension>()!;
     final themeFontStyle = Theme.of(context).extension<TextStyleExtension>()!;
 
-    return Container(
-      constraints: BoxConstraints(
-        minWidth: size.width * 0.01,
-        maxWidth: size.width * 0.2,
-      ),
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: isMessageReceived
-            ? themeColor.messageBubbleReceivedColor
-            : themeColor.messageBubbleSendedColor,
-        borderRadius: isMessageReceived
-            ? const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-                topRight: Radius.circular(20),
-              )
-            : const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
-              ),
-      ),
-      child: Text(
-        messageContent,
-        style: themeFontStyle.messageContentTextStyle.copyWith(
-          fontSize: 16,
-          color: themeColor.whiteTextColor,
+    return Row(
+      mainAxisAlignment:
+          isMessageReceived ? MainAxisAlignment.start : MainAxisAlignment.end,
+      children: [
+        Container(
+          constraints: BoxConstraints(
+            minWidth: size.width * 0.01,
+            maxWidth: size.width * 0.2,
+          ),
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: isMessageReceived
+                ? themeColor.messageBubbleReceivedColor
+                : themeColor.messageBubbleSendedColor,
+            borderRadius: isMessageReceived
+                ? const BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  )
+                : const BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+          ),
+          child: Text(
+            messageContent,
+            style: themeFontStyle.messageContentTextStyle.copyWith(
+              fontSize: 16,
+              color: themeColor.whiteTextColor,
+            ),
+            textAlign: TextAlign.start,
+            maxLines: 10,
+            softWrap: true,
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
-        textAlign: TextAlign.start,
-        maxLines: 10,
-        softWrap: true,
-        overflow: TextOverflow.ellipsis,
-      ),
+      ],
     );
   }
 }
